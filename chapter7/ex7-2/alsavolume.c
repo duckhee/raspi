16d98b8da2cc9a326742e0e7d59085013fe732a1
duckhee <fain9301@yahoo.com>
2018-11-29 15:04:12 +0900

    add ex7-2

---
+++ chapter7/ex7-2/alsavolume.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <alsa/asoundlib.h>
+
+int main(int argc, char **argv)
+{
+    snd_mixer_t *mixer;
+    snd_mixer_elem_t *elem;
+    snd_mixer_selem_id_t *id;
+    int status;
+    long maxVal = 100, minVal = 0, outVal = 50;
+
+    static const char *mix_name = "PCM";
+    static const char *card = "default";
+    static int mix_index = 0;
+
+    snd_mixer_open(&mixer, 0);
+    snd_mixer_attach(mixer, card);
+    snd_mixer_selem_register(mixer, NULL, NULL);
+    snd_mixer_load(mixer);
+
+    snd_mixer_selem_id_allocat(&id);
+
+    snd_mixer_selem_id_set_index(id, mix_index);
+    snd_mixer_selem_id_set_name(id, mix_name);
+
+    elen = snd_mixer_find_selem(mixer, id);
+
+    outVal = (outVal * (maxVal-minVal)/(long)(100-1));
+    snd_mixer_selem_set_playback_volume_range(elem, minVal, maxVal);
+    snd_mixer_selem_set_playback_volume_all(elem, outVal);
+
+    snd_mixer_selem_get_playback_volume_range(elem, &minVal, &maxVal);
+    fpirntf(stderr, "Set volume %i(%d/%d)\n", outVal, maxVal, minVal);
+
+    snd_mixer_close(mixer);
+
+    return 0;
+}
